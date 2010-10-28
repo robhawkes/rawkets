229bf30db26f14163e85a2d08055f6e0147e4c08
Rob Hawkes <rob.hawkes@gmail.com>
2010-10-28 21:14:39 +0100

    - Message protocol labels are now 1 character long

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,4 @@
 $(function() {
-	// Firefox support
-	//window.WEB_SOCKET_DEBUG = true;
-	//window.WEB_SOCKET_SWF_LOCATION = "/experiments/rawkets/js/socket.io/lib/vendor/web-socket-js/WebSocketMain.swf";
-	//io.setPath("/experiments/rawkets/js/socket.io/");
-
 	var game;
 	
 	/**
@@ -35,10 +30,6 @@ $(function() {
 				 // Horrible passing of game object due to event closure
 				 .bind("keydown", {self: game}, game.movePlayer)
 				 .bind("keyup", {self: game}, game.haltPlayer);
-		
-		/*$(window).bind("mouseup", function(e) {
-			game.socket.send(Game.formatMessage("testing", {x: 123, y: 456}));
-		});*/
 	};
 	
 	// Initialise client-side functionality
