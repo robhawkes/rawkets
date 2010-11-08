acff683741809743387758623056a02ee8aae51c
Rob Hawkes <rob.hawkes@gmail.com>
2010-11-08 10:15:39 +0000

    - Added Google Analytics

--- public/index.php
+++ public/index.php
@@ -145,6 +145,19 @@ try {
 		<script type="text/javascript">
 		try { Observerapp.record("4cc2aa5ba7b560500a05879c"); } catch(e) {};
 		</script>
+		
+		<!-- Google Analytics -->
+		<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-19570160-1']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+		</script>
 	</body>
 
 </html>
