cb35c1883bacf7e8abc576cf397ae0f88830d3f0
Rob Hawkes <rob.hawkes@gmail.com>
2010-11-10 19:56:21 +0000

    - Manually set WebSocket connections to point to dedicated server

--- public/js/Socket.js
+++ public/js/Socket.js
@@ -10,7 +10,8 @@
  */
 var Socket = function() {
 	// Initialise WebSocket
-	this.socket = new WebSocket("ws://"+document.location.host+":8000");
+	//this.socket = new WebSocket("ws://"+document.location.host+":8000");
+	this.socket = new WebSocket("ws://socket.rawkets.com:80"); // Testing new server with port 80 socket connection
 	
 	// Return socket object
 	return this.socket;
