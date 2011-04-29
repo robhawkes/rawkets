96175db6b73a3047fb16ea2a3a8b9cb54cc8a8af
Rob Hawkes <rob.hawkes@gmail.com>
2011-04-30 00:51:41 +0100

    - Added netgraph for diagnostics - Replaced clone function, but it's still not reliable - Added full keyboard controls for player - Changed player to look like a rocket with flame - Added bullets, but currently has a bug where they sometimes stay on screen

--- Server/rawkets.js
+++ Server/rawkets.js
@@ -1,5 +1,5 @@
 // Funky Object clone method
-Object.prototype.clone = function() {
+/*Object.prototype.clone = function() {
 	var newObj = (this instanceof Array) ? [] : {};
 	
 	for (i in this) {
@@ -13,6 +13,30 @@ Object.prototype.clone = function() {
 	};
 		
 	return newObj;
+};*/
+
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Array
+    if (obj instanceof Array) {
+        var copy = [], i, len = obj.length;
+        for (i = 0; i < len; ++i) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    };
+
+    // Handle Object
+    if (obj instanceof Object) {
+        var copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        }
+        return copy;
+    };
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
 };
 
 // Vector object
@@ -29,17 +53,22 @@ var Vector = function(opts) {
 
 // Player state object
 var PlayerState = function(opts) {
-	// Private variables
-	var keys = {left: false, right: false, up: false};
-	
 	// Public variables
-	var pos = new Vector({x: opts.x || 0, y: opts.y || 0}),
-		acc = new Vector({x: opts.acc.x || 0, y: opts.acc.y || 0});
+	var currentKeys = {left: false, right: false, up: false, down: false, space: false},
+		pos = new Vector({x: opts.x, y: opts.y}),
+		thrust = 1500,
+		//acc = new Vector({x: opts.acc.x || 0, y: opts.acc.y || 0}),
+		angle = 0,
+		moving = false,
+		alive = true;
 		
 	return {
 		pos: pos,
-		acc: acc,
-		keys: keys
+		thrust: thrust,
+		angle: angle,
+		moving: moving,
+		currentKeys: currentKeys,
+		alive: alive
 	};
 };
 
@@ -47,18 +76,26 @@ var PlayerState = function(opts) {
 var Player = function(opts) {
 	// Public variables
 	var id = opts.id || false,
-		currentState = opts.state || new PlayerState({x: opts.x || 0, y: opts.y || 0, acc: {x: opts.acc.x || 0, y: opts.acc.y || 0}}),
-		previousState = currentState.clone();
+		currentState = new PlayerState({x: opts.x, y: opts.y}),
+		previousState = clone(currentState),
+		weaponsHot = true;
+		
+	//console.log(currentState.pos.x);
 		
 	// Public methods
 	var update = function(dtdt) {
-		// Update previous state
-		previousState = currentState.clone();
+		// Update previous state (fix the clone issues)
+		previousState.currentKeys = clone(currentState.currentKeys);
+		previousState.pos = clone(currentState.pos);
+		previousState.angle = currentState.angle;
+		previousState.moving = currentState.moving;
+		previousState.alive = currentState.alive;
+		//console.log(previousState.pos.x);
 		
 		// Do stuff depending on what keys are currently pressed down
-		if (currentState.keys.left) {
+		/*if (currentState.currentKeys.left) {
 			currentState.acc.x = -1500;
-		} else if (currentState.keys.right) {
+		} else if (currentState.currentKeys.right) {
 			currentState.acc.x = 1500;
 		} else {
 			if (Math.abs(currentState.acc.x) < 1) {
@@ -66,23 +103,31 @@ var Player = function(opts) {
 			} else {
 				currentState.acc.x *= 0.96;
 			}
+		};*/
+		
+		if (!currentState.alive) {
+			return;
 		};
 		
-		if (currentState.keys.up) {
-			currentState.acc.y = -1500;
-		} else if (currentState.keys.down) {
-			currentState.acc.y = 1500;
-		} else {
-			if (Math.abs(currentState.acc.y) < 1) {
-				currentState.acc.y = 0;
-			} else {
-				currentState.acc.y *= 0.96;
-			}
+		if (currentState.currentKeys.left) {
+			currentState.angle -= 0.09;
+		};
+		
+		if (currentState.currentKeys.right) {
+			currentState.angle += 0.09;
+		};
+		
+		var acc = new Vector({x: 0, y: 0});
+		currentState.moving = false;
+		if (currentState.currentKeys.up) {
+			currentState.moving = true;
+			acc.x = Math.cos(currentState.angle)*currentState.thrust;
+			acc.y = Math.sin(currentState.angle)*currentState.thrust;
 		};
 		
 		// Verlet integration (http://www.gotoandplay.it/_articles/2005/08/advCharPhysics.php)
-		currentState.pos.x = 2 * currentState.pos.x - previousState.pos.x + currentState.acc.x * dtdt;
-		currentState.pos.y = 2 * currentState.pos.y - previousState.pos.y + currentState.acc.y * dtdt;
+		currentState.pos.x = 2 * currentState.pos.x - previousState.pos.x + acc.x * dtdt;
+		currentState.pos.y = 2 * currentState.pos.y - previousState.pos.y + acc.y * dtdt;
 		
 		if (!withinWorldBounds(currentState.pos.x, currentState.pos.y)) {
 			if (currentState.pos.x > worldWidth)
@@ -97,13 +142,89 @@ var Player = function(opts) {
 			if (currentState.pos.y < 0)
 				currentState.pos.y = 0;
 		};
+		
+		//console.log(currentState.pos.x);
 	};
-	
+
 	return {
 		id: id, // Should probably be made read-only
 		update: update,
 		currentState: currentState,
-		previousState: previousState
+		previousState: previousState,
+		weaponsHot: weaponsHot
+	};
+};
+
+var killPlayer = function(playerId) {
+	var player = playerById(playerId);
+	if (player) {
+		//console.log("Kill player", player.id);
+		player.currentState.moving = false;
+		player.currentState.alive = false;
+		var self = player;
+		setTimeout(function() {
+			//console.log("Revive player", self.id);
+			self.currentState.alive = true;
+			
+			var client = socket.clients[player.id];
+			if (client) {
+				client.broadcast(formatMessage(MESSAGE_TYPE_UPDATE_REMOTE_PLAYER_STATE, {id: player.id, state: player.currentState}));
+				client.send(formatMessage(MESSAGE_TYPE_UPDATE_LOCAL_PLAYER_POSITION, {id: player.id, pos: player.currentState.pos, angle: player.currentState.angle, moving: player.currentState.moving, alive: player.currentState.alive}));
+			};
+		}, 2000);
+	};
+};
+
+var setBulletTimer = function(playerId) {
+	var player = playerById(playerId);
+	if (player) {
+		player.weaponsHot = false;
+		var self = player;
+		setTimeout(function() {
+			//console.log("Weapons hot", self.id);
+			self.weaponsHot = true;
+		}, 350);
+	};
+};
+
+// Bullet state object
+var BulletState = function(opts) {
+	// Public variables
+	var pos = new Vector({x: opts.x, y: opts.y}),
+		vel = 6000,
+		acc = new Vector({x: Math.cos(opts.angle)*vel, y:Math.sin(opts.angle)*vel}),
+		age = 0;
+		
+	return {
+		pos: pos,
+		acc: acc,
+		age: age
+	};
+};
+
+// Bullet object
+var Bullet = function(opts) {
+	// Public variables
+	var id = opts.id || false,
+		playerId = opts.playerId || false,
+		currentState = new BulletState({x: opts.x, y: opts.y, angle: opts.angle}),
+		previousState = clone(currentState);
+		
+	// Public methods
+	var update = function(dtdt) {
+		// Update previous state
+		previousState = clone(currentState);
+		currentState.age++;
+		
+		currentState.pos.x = 2 * currentState.pos.x - previousState.pos.x + currentState.acc.x * dtdt;
+		currentState.pos.y = 2 * currentState.pos.y - previousState.pos.y + currentState.acc.y * dtdt;
+	};
+	
+	return {
+		id: id,
+		playerId: playerId,
+		currentState: currentState,
+		update: update
 	};
 };
 
@@ -133,13 +254,25 @@ function withinWorldBounds(x, y) {
 	return false;
 };
 
+// Ping helper
+function sendPing(client) {
+	setTimeout(function() {
+		var timestamp = new Date().getTime();
+		client.send(formatMessage(MESSAGE_TYPE_PING, {t: timestamp.toString()}));
+	}, 1000);
+};
+
 // Message types
 var MESSAGE_TYPE_UPDATE_REMOTE_PLAYER_STATE = 0,
 	MESSAGE_TYPE_NEW_PLAYER = 1,
 	MESSAGE_TYPE_REMOVE_PLAYER = 2,
 	MESSAGE_TYPE_ENABLE_PLAYER_KEY = 3,
 	MESSAGE_TYPE_DISABLE_PLAYER_KEY = 4,
-	MESSAGE_TYPE_UPDATE_LOCAL_PLAYER_POSITION = 5;
+	MESSAGE_TYPE_UPDATE_LOCAL_PLAYER_POSITION = 5,
+	MESSAGE_TYPE_PING = 6,
+	MESSAGE_TYPE_NEW_BULLET = 7,
+	MESSAGE_TYPE_UPDATE_BULLET_STATE = 8,
+	MESSAGE_TYPE_REMOVE_BULLET = 9;
 
 // Start of main game setup
 var http = require("http"), 
@@ -150,6 +283,7 @@ var http = require("http"),
 	
 	// Run game
 	runUpdate = true,
+	serverStart = new Date().getTime(),
 	
 	// Fixed physics update
 	t = 0,
@@ -173,6 +307,9 @@ var http = require("http"),
 	
 	// Players
 	players = [],
+	
+	// Weapons
+	bullets = [], // Add this into the player object?
 	
 	// Communication
 	msgInQueue = [], // Incoming messages
@@ -257,19 +394,110 @@ function update() {
 	//updatePhysics();
 	
 	// Update every single player in the game
-	var i, player, client, numPlayers = players.length;
-	for (i = 0; i < numPlayers; i++) {
+	var i, player, bullet, bulletPos, timestamp, client, playerCount = players.length;
+	for (i = 0; i < playerCount; i++) {
 		player = players[i];
 		
 		if (player) {
 			player.update(dtdt);
+			
+			//console.log(player.previousState.pos.x);
+			//console.log(player);
+			
+			if (player.currentState.alive && player.currentState.currentKeys.space) {
+				//console.log(player.weaponsHot, player.id);
+				if (player.weaponsHot) {
+					bulletPos = new Vector({x: 0, y: 0});
+					//console.log("FIRE");
+					timestamp = new Date().getTime();
+
+					bulletPos.x = player.currentState.pos.x+(Math.cos(player.currentState.angle)*7);
+					bulletPos.y = player.currentState.pos.y+(Math.sin(player.currentState.angle)*7);
+					
+					var id = timestamp+player.id.toString()+(Math.round(Math.random()*99));
+					
+					bullet = new Bullet({id: id, playerId: player.id, x: bulletPos.x, y: bulletPos.y, angle: player.currentState.angle});
+					bullets.push(bullet);
+					
+					socket.broadcast(formatMessage(MESSAGE_TYPE_NEW_BULLET, {id: id, x: bullet.currentState.pos.x, y: bullet.currentState.pos.y}));
+					
+					setBulletTimer(players[i].id);
+				};
+			};
 		
 			client = socket.clients[player.id];
-			
+
 			if (client) {
-				client.broadcast(formatMessage(MESSAGE_TYPE_UPDATE_REMOTE_PLAYER_STATE, {id: player.id, state: player.currentState}));
-				client.send(formatMessage(MESSAGE_TYPE_UPDATE_LOCAL_PLAYER_POSITION, {id: player.id, pos: player.currentState.pos}));
+				if (Math.abs(player.previousState.pos.x - player.currentState.pos.x) > 0.1 || 
+					Math.abs(player.previousState.pos.y - player.currentState.pos.y) > 0.1 || 
+					Math.abs(player.previousState.angle != player.currentState.angle) || 
+					player.previousState.moving != player.currentState.moving) {
+					//console.log("Update");
+					client.broadcast(formatMessage(MESSAGE_TYPE_UPDATE_REMOTE_PLAYER_STATE, {id: player.id, state: player.currentState}));
+					client.send(formatMessage(MESSAGE_TYPE_UPDATE_LOCAL_PLAYER_POSITION, {id: player.id, pos: player.currentState.pos, angle: player.currentState.angle, moving: player.currentState.moving, alive: player.currentState.alive}));
+				};
 			};
+		};
+	};
+	
+	// Update every single bullet in the game
+	var b, deadBullets = [], deadPlayers = [], bulletCount = bullets.length;
+	//console.log(bulletCount);
+	for (b = 0; b < bulletCount; b++) {
+		bullet = bullets[b];
+		
+		if (bullet) {
+			if (bullet.currentState.age > 100) {
+				deadBullets.push(bullet);
+				continue;
+			};
+			
+			bullet.update(dtdt);
+			
+			// Check for kills
+			for (var p = 0; p < playerCount; p++) {
+				player = players[p];
+
+				if (player) {
+					var dx = bullet.currentState.pos.x - player.currentState.pos.x;
+					var dy = bullet.currentState.pos.y - player.currentState.pos.y;
+					var dd = (dx * dx) + (dy * dy);
+					var d = Math.sqrt(dd);
+			
+					if (d < 10) {
+						deadPlayers.push(player);
+						deadBullets.push(bullet);
+						continue;
+					};
+				};
+			};
+			
+			socket.broadcast(formatMessage(MESSAGE_TYPE_UPDATE_BULLET_STATE, {id: bullet.id, x: bullet.currentState.pos.x, y: bullet.currentState.pos.y}));
+		};
+	};
+	
+	var dp, deadPlayerCount = deadPlayers.length;
+	for (dp = 0; dp < deadPlayerCount; dp++) {
+		player = deadPlayers[dp];
+		
+		if (player) {
+			killPlayer(player.id);
+			
+			client = socket.clients[player.id];
+			client.broadcast(formatMessage(MESSAGE_TYPE_UPDATE_REMOTE_PLAYER_STATE, {id: player.id, state: player.currentState}));
+			client.send(formatMessage(MESSAGE_TYPE_UPDATE_LOCAL_PLAYER_POSITION, {id: player.id, pos: player.currentState.pos, angle: player.currentState.angle, moving: player.currentState.moving, alive: player.currentState.alive}));
+		};
+	};
+	
+	// Remove dead bullets
+	var db, deadBulletCount = deadBullets.length;
+	for (db = 0; db < deadBulletCount; db++) {
+		bullet = deadBullets[db];
+		
+		if (bullet) {
+			socket.broadcast(formatMessage(MESSAGE_TYPE_REMOVE_BULLET, {id: bullet.id}));
+			bullets.splice(db, 1);
+			//console.log("Remove bullet", bullets.length);
 		};
 	};
 	
@@ -307,17 +535,44 @@ function unqueueIncomingMessages(msgQueue) {
 		msg = BISON.decode(data.msg);
 		
 		// Only deal with messages using the correct protocol
-		if (msg.type !== undefined) {
+		if (msg !== undefined && msg.type !== undefined) {
 			var player;
 			switch (msg.type) {
+				case MESSAGE_TYPE_PING:
+					player = players[indexOfByPlayerId(client.sessionId)];
+					
+					if (player == null) {
+						break;
+					};
+					
+					player.age = 0; // Player is active
+					
+					var newTimestamp = new Date().getTime();
+					//console.log("Round trip: "+(newTimestamp-data.ts)+"ms", client.sessionId);
+					var ping = newTimestamp-msg.t;
+					
+					// Send ping back to player
+					client.send(formatMessage(MESSAGE_TYPE_PING, {i: player.id, p: ping}));
+					
+					// Broadcast ping to other players
+					//client.broadcast(formatMessage(MESSAGE_TYPE_UPDATE_PING, {i: client.id, p: ping}));
+					
+					// Log ping to server after every 10 seconds
+					if ((newTimestamp-serverStart) % 5000 <= 3000) {
+						console.log("PING ["+client.sessionId+"]: "+ping);
+					};
+					
+					// Request a new ping
+					sendPing(client);
+					break;
 				case MESSAGE_TYPE_NEW_PLAYER:
 					console.log("Adding new player: ", client.sessionId);
-					players.push(new Player({id: client.sessionId, state: msg.state}));
+					players.push(new Player({id: client.sessionId, x: msg.state.pos.x, y: msg.state.pos.y}));
 					console.log("Players connected: ", players.length);
 					
 					// Move into a queueing system
 					client.broadcast(formatMessage(MESSAGE_TYPE_NEW_PLAYER, {id: client.sessionId, state: msg.state}));
-					
+					sendPing(client);
 					break;
 				/*case MESSAGE_TYPE_UPDATE_REMOTE_PLAYER_STATE:
 					console.log("Update player state: ", client.sessionId);
@@ -328,39 +583,47 @@ function unqueueIncomingMessages(msgQueue) {
 					};
 					break;*/
 				case MESSAGE_TYPE_ENABLE_PLAYER_KEY:
+					//console.log("Enable key: ", msg.key);
 					player = playerById(client.sessionId);
 					if (player) {
 						switch (msg.key) {
+							case 32: // Space
+								player.currentState.currentKeys.space = true;
+								break;
 							case 37: // Left
-								player.currentState.keys.left = true;
+								player.currentState.currentKeys.left = true;
 								break;
 							case 38: // Up
-								player.currentState.keys.up = true;
+								player.currentState.currentKeys.up = true;
 								break;
 							case 39: // Right
-								player.currentState.keys.right = true;
+								player.currentState.currentKeys.right = true;
 								break;
 							case 40: // Down
-								player.currentState.keys.down = true;
+								player.currentState.currentKeys.down = true;
 								break;
 						};
 					};
 					break;
 				case MESSAGE_TYPE_DISABLE_PLAYER_KEY:
+					//console.log("Disable key: ", msg.key);
 					player = playerById(client.sessionId);
 					if (player) {
 						switch (msg.key) {
+							case 32: // Space
+								player.currentState.currentKeys.space = false;
+								break;
 							case 37: // Left
-								player.currentState.keys.left = false;
+								player.currentState.currentKeys.left = false;
 								break;
 							case 38: // Up
-								player.currentState.keys.up = false;
+								player.currentState.currentKeys.up = false;
 								break;
 							case 39: // Right
-								player.currentState.keys.right = false;
+								player.currentState.currentKeys.right = false;
 								break;
 							case 40: // Down
-								player.currentState.keys.down = false;
+								player.currentState.currentKeys.down = false;
 								break;
 						};
 					};
@@ -420,62 +683,28 @@ function indexOfByPlayerId(id) {
 	return false;
 };
 
-// Start main update loop
-update();
-
-// Physics update loop (fixed timestep)
-/*function updatePhysics() {
-	//previousState = {x: currentState.x};
-	
-	// Verlet integration (http://www.gotoandplay.it/_articles/2005/08/advCharPhysics.php)
-	//currentState.x = 2 * currentState.x - previousState.x + acceleration * dtdt;
-	
-	t += dt;
-};*/
-
 /*
-// Physics update loop (semi-fixed timestep)
-function updatePhysics() {
-	// Temporary variables
-	var newTime = Date.now();
-		frameTime = (newTime - currentTime)/100;
-		//accumlatorCount = 0;
-		
-	//console.log(frameTime);
-	
-	// Limit frame time to avoid spiral of death
-	if (frameTime > 0.5) {
-		//console.log(frameTime);
-		frameTime = 0.5;
+// Find bullet by ID
+function bulletById(id) {
+	for (var i = 0; i < bullets.length; i++) {
+		if (bullets[i].id == id)
+			return bullets[i];
 	};
-	
-	currentTime = newTime;
-	accumulator += frameTime;
-	
-	while (accumulator >= dt) {
-		previousState = {x: currentState.x};
-		
-		// Verlet integration (http://www.gotoandplay.it/_articles/2005/08/advCharPhysics.php)
-		//integrate(currentState, t, dt);
-		currentState.x = 2 * currentState.x - previousState.x + acceleration * dtdt;
-		
-		accumulator -= dt;
-		//console.log(accumulator);
-		//accumlatorCount++;
-	};
-	
-	//console.log(accumlatorCount);
 
-	var alpha = accumulator/dt;
-	
-	// Interpolate (Gafferongames)
-	state = {x: 0};
-	state.x = currentState.x*alpha + previousState.x*(1.0 - alpha);
-	
-	if (runUpdate) {
-		setTimeout(updatePhysics, 1000/60);
-	};
+	return false;
 };
 
-updatePhysics();
+// Find bullet index by ID
+function indexOfByBulletId(id) {
+	for (var i = 0; i < bullets.length; i++) {
+		if (bullets[i].id == id) {
+			return i;
+		};
+	};
+
+	return false;
+};
 */
+
+// Start main update loop
+update();
