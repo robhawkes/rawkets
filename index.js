67d5bbd450a8a218a1c4179fecffb4b8f177564e
Rob Hawkes <rob@rawkes.com>
2012-01-14 22:03:36 +0000

    Initial hook.io setup

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-var rawkets = require("./server/index");
-rawkets.init();
+var Hook = require("hook.io").Hook,
+	RawketsClient = require("hook.io-rawkets-client").RawketsClient;
+
+// Initialise hook server
+var hook = new Hook( {
+	name: "vanilla-hook",
+	silent: true
+});
+
+hook.on("hook::ready", function() {
+	// Spawn rawkets-client hook
+	hook.spawn([{
+		type: "rawkets-client",
+		name: "rawkets-client",
+		silent: false
+	}]);
+});
+
+// Start hook server
+hook.start();
+
+// var rawkets = require("./server/index");
+// rawkets.init();
